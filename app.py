50f2190ca841a71ee8db16ed1d96ee72d00eb4b8
ivanovich1071 <163851033+ivanovich1071@users.noreply.github.com>
2024-07-24 14:28:20 +0300

    add 3 peges

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/menu')
+@app.route('/menu/')
 def menu():
     return render_template('menu.html')
 
-@app.route('/contact')
+@app.route('/contact/')
 def contact():
     return render_template('contact.html')
 
--- main.py
+++ main.py
@@ -9,16 +9,6 @@ def home():
     formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
     return f"<h1>Текущие дата и время: {formatted_time}</h1>"
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/menu')
-def menu():
-    return render_template('menu.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 if __name__ == '__main__':
     app.run(debug=True)
